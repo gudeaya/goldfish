8d9b1a6c3591ed71a88e3c5d1cf5decad8bf0387
gudeaya <a.0624.dev@gmail.com>
2023-01-05 14:29:43 +0900

    title 変更

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <Container>
       <Head>
-        <title>goldfish</title>
+        <title>Goldfish</title>
         <meta name={'viewport'} content={'width=device-width,initial-scale=1.0'} />
         <meta name={'description'} content={'我が家の金魚を紹介するだけのサイト'} />
         <link rel={'icon'} href={'/favicon.ico'} />
